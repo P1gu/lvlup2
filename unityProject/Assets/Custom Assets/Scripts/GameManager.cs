18908ba9cad5e696b047448f681dcbdb553a4538
P1gu <michaeldasilva96@gmail.com>
2016-02-18 21:30:05 +0100

    add structure

---
+++ unityProject/Assets/Custom Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    enum EtatDuJeu {
+        MenuDuJeux
+    };
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
