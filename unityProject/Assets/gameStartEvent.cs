ee579efad4c1d3046de5fb1cf1cb719630a24a44
Dragoo <doriansporefan@gmail.com>
2016-02-19 21:43:14 +0100

    Menu avec boutons animés.

---
+++ unityProject/Assets/gameStartEvent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameStartEvent : MonoBehaviour {
+
+    public void StartGame()
+    {
+        Application.LoadLevel("Jeu");
+    }
+
+}
