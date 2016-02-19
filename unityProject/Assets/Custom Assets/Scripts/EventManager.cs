8a8e93a2098429d711be3ba89d3eb59cb8929864
Dragoo <doriansporefan@gmail.com>
2016-02-19 15:27:00 +0100

    Music manager + groupage des sons en musiques et effets

--- unityProject/Assets/Custom Assets/Scripts/EventManager.cs
+++ unityProject/Assets/Custom Assets/Scripts/EventManager.cs
@@ -9,6 +9,23 @@ public class EventManager : MonoBehaviour {
     public delegate void UnAventurierEstMort();
     public static event UnAventurierEstMort OnUnAventurierEstMort;
 
+    public delegate void Phase1Start();
+    public static event Phase1Start OnPhase1Start;
+
+    public delegate void ReadyForPhase2();
+    public static event ReadyForPhase2 OnReadyForPhase2;
+
+    public delegate void Phase2Start();
+    public static event Phase2Start OnPhase2Start;
+
+    public delegate void GameOver();
+    public static event GameOver OnGameOver;
+
+    public delegate void Victory();
+    public static event Victory OnVictory;
+
+    public delegate void EnterMenu();
+    public static event EnterMenu OnEnterMenu;
 
 
     // Use this for initialization
